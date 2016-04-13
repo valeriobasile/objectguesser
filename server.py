ec5530413d46fea4f1791e452cad14b5e3825782
valeriobasile <valeriobasile@gmail.com>
2016-04-13 11:44:20 +0200

    reorganized files

--- server.py
+++ server.py
@@ -15,29 +15,6 @@ urls = (
 app = web.application(urls, globals())
 
 
-# read NASARI vectors
-log.info('reading vectors')
-vectors = dict()
-with open('knowrob_vectors.txt') as f:
-    for line in f:
-        fields = line.rstrip().split(' ')
-        label = fields[1]
-        coordinates = map(eval, fields[2:])
-        vectors[label] = coordinates
-
-# read objects frequencies
-log.info('reading frequencies')
-frequencies = dict()
-with open('knowrob_frequencies.txt') as f:
-    for line in f:
-        fields = line.rstrip().split(' ')
-        label = fields[1]
-        frequency = eval(fields[0])
-        frequencies[label] = frequency
-# normalizing
-#m = float(max(frequencies.values()))
-#frequencies = {k: float(v)/m for k, v in frequencies.items()}
-
 def cosine_similarity(v1,v2):
     "compute cosine similarity of v1 to v2: (v1 dot v2)/{||v1||*||v2||)"
     sumxx, sumxy, sumyy = 0, 0, 0
@@ -67,25 +44,35 @@ def parse_query(data, vicinity):
     distances = ["near_{0}".format(x) for x in range(vicinity+1)]
 
     query_objects = query['local_objects']
-    query_objects.append([query["context_room_label"],1,"","near_0"])
-    query_objects.append([query["context_surface_label"],1,"","near_0"])
+    #query_objects.append([query["context_room_label"],1,"","near_0"])
+    #query_objects.append([query["context_surface_label"],1,"","near_0"])
     # fix missing objects
-    objects = filter(lambda x: x[0] in vectors, query_objects)
+    objects = filter(lambda x: x[0] in vectors['tools'], query_objects)
 
     # filter for distance
     objects = filter(lambda x: x[3] in distances, objects)
 
+    query_cooccurrences = query['co_occurrences']
+    cooccurrences = filter(lambda x: x[0] in vectors['tools'], query_cooccurrences)
+    cooccurrences = filter(lambda x: x[2] in distances, cooccurrences)
+
     labels = map(lambda x: x[0], objects)
 
-    return objects, labels
+    room = query['context_room_label']
+    surface = query['context_surface_label']
+
+    return objects, labels, cooccurrences, room, surface
 
 def relatedness(o, objects, method):
     if o in objects:
         return 0
 
-    m = map(lambda x: cosine_similarity(vectors[o], vectors[x]), objects)
+    m = map(lambda x: cosine_similarity(vectors['tools'][o], vectors['tools'][x]), objects)
     #m = [((1.0-weights)*i)+(weights*frequencies[o]) for i in m]
 
+    print o
+    print objects
+    print m
     if len(m)>1:
         if method == 'prod':
             return reduce(lambda x,y:x*y, m)
@@ -100,24 +87,60 @@ def relatedness(o, objects, method):
     else:
         return 0.0
 
+# read NASARI vectors
+vectors = dict()
+frequencies = dict()
+for cat in ['tools', 'rooms', 'furniture']:
+    log.info('reading vectors')
+    vectors[cat] = dict()
+    with open('vectors/{0}_vectors.txt'.format(cat)) as f:
+        for line in f:
+            fields = line.rstrip().split(' ')
+            label = fields[1]
+            coordinates = map(eval, fields[2:])
+            vectors[cat][label] = coordinates
+
+    # read objects frequencies
+    log.info('reading frequencies')
+    frequencies[cat] = dict()
+    with open('frequencies/{0}_frequencies.txt'.format(cat)) as f:
+        for line in f:
+            fields = line.rstrip().split(' ')
+            label = fields[1]
+            frequency = eval(fields[0])
+            frequencies[cat][label] = frequency
+    # normalizing
+    #m = float(max(frequencies.values()))
+    #frequencies = {k: float(v)/m for k, v in frequencies.items()}
+
 class guess:
     def POST(self):
         # default values
-        args = web.input(n='10', p='2', m='max', t='0')
+        args = web.input(n='10', p='2', m='prod', t='0')
         n = eval(args.n)
         proximity = eval(args.p)
         threshold = eval(args.t)
-        objects, labels = parse_query(web.data(), proximity)
+        objects, labels, cooccurrences, room, surface = parse_query(web.data(), proximity)
 
         result = []
-        for o in vectors.keys():
+        for o in vectors['tools'].keys():
             r = relatedness(o, labels, method=args.m)
-            if o in frequencies:
-                freq = frequencies[o]
+
+            if room in vectors['rooms']:
+                roomrel = cosine_similarity(vectors['tools'][o], vectors['rooms'][room])
+            else:
+                roomrel = 0.0
+            if surface in vectors['furniture']:
+                surfrel = cosine_similarity(vectors['tools'][o], vectors['furniture'][surface])
+            else:
+                surfrel = 0.0
+
+            if o in frequencies['tools']:
+                freq = frequencies['tools'][o]
             else:
 	            freq = 0
             if freq >= threshold:
-                result.append({'object':o, 'relatedness':r, 'frequency':freq})
+                result.append({'object':o, 'relatedness':r, 'frequency':freq, 'roomrel':roomrel, 'surfrel':surfrel})
         result_sorted = list(reversed(sorted(result, key=lambda x: x['relatedness'])))
 
         return json.dumps(result_sorted[:n])
